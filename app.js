d3cf5e56210a8696f1a5f90c59243c507f9684f1
Ehiorobu Gideon Miguel <ehigideon517@gmail.com>
2025-10-05 00:54:17 +0100

    Chore: update app.js

--- app.js
+++ app.js
@@ -1,27 +1,31 @@
-const express = require("express")
-const mongoose = require("mongoose")
-const mongodb = require("mongodb")
-const cookieParser = require("cookie-parser")
-const authRoute = require("./routers/route")
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const authRoute = require("./routers/route");
+require("dotenv").config();
 
-require("dotenv").config()
-const app = express()
-app.use(express.json())
-app.use(express.urlencoded({extended: false}))
-app.use(cookieParser())
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
+const dbUri = process.env.MONGODB_URI;
 
+if (!dbUri) {
+    console.error("MongoDB URI is not set");
+    process.exit(1);
+}
 
-mongoose.connect(process.env.MONGODB_URI)
-.then(()=>{
-    console.log("connected to Database")
-    app.listen(3000, ()=>{
-        console.log("connection to Port 3000")
+mongoose.connect(dbUri)
+    .then(() => {
+        console.log("Connected to Database");
+        app.listen(3000, () => {
+            console.log("Server is listening on Port 3000");
+        });
     })
-})
-.catch((error)=>{
-console.error("error occured connecting to Database", error)
-process.exit(1)
-})
+    .catch((error) => {
+        console.error("Error occurred connecting to Database", error);
+        process.exit(1);
+    });
 
-app.use(authRoute)
+app.use(authRoute);
